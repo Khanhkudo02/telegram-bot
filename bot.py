97c4ca97ab823a776a4c32d0b8d702641e245a52
Khanhkudo02 <113323275+Khanhkudo02@users.noreply.github.com>
2026-03-16 18:28:08 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 from openai import OpenAI
 
 BOT_TOKEN = "8668169502:AAEL5CWwL29nFRRx6gXJGfZ86YcYs7BALsw"
-client = OpenAI(api_key=os.getenv("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"))
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
