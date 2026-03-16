ab987f11a34a660c934d70e023834eeacf0b9553
Khanhkudo02 <113323275+Khanhkudo02@users.noreply.github.com>
2026-03-16 18:24:38 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,20 @@
 import telebot
+import os
+from openai import OpenAI
 
-TOKEN = "8668169502:AAEL5CWwL29nFRRx6gXJGfZ86YcYs7BALsw"
+BOT_TOKEN = "8668169502:AAEL5CWwL29nFRRx6gXJGfZ86YcYs7BALsw"
+client = OpenAI(api_key=os.getenv("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"))
 
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(func=lambda message: True)
-def reply(message):
-    bot.reply_to(message, "Xin chào Khánh 👋")
+def chat(message):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": message.text}]
+    )
+
+    reply = response.choices[0].message.content
+    bot.reply_to(message, reply)
 
 bot.polling()
