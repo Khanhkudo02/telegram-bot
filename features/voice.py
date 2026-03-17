2ece237840705462766765997aeab4ae58895b56
Khanhkudo02 <113323275+Khanhkudo02@users.noreply.github.com>
2026-03-17 11:20:58 +0700

    Update voice.py

--- features/voice.py
+++ features/voice.py
@@ -1,1 +1,15 @@
+import edge_tts
+import asyncio
 
+async def _voice(text, chat_id, bot):
+    communicate = edge_tts.Communicate(text, "vi-VN-HoaiMyNeural")
+    await communicate.save("voice.mp3")
+
+    with open("voice.mp3", "rb") as v:
+        bot.send_voice(chat_id, v)
+
+def send_voice(text, chat_id, bot):
+    try:
+        asyncio.run(_voice(text, chat_id, bot))
+    except:
+        pass
