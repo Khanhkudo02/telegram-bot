93964d662db474a538ca6d52714c1fb87f094263
Khanhkudo02 <113323275+Khanhkudo02@users.noreply.github.com>
2026-03-17 11:21:47 +0700

    Update search.py

--- features/search.py
+++ features/search.py
@@ -1,1 +1,18 @@
+from duckduckgo_search import DDGS
 
+def search_web(query):
+    try:
+        text = ""
+        with DDGS() as ddgs:
+            results = list(ddgs.text(query, max_results=5))
+
+        if not results:
+            return "❌ Không tìm thấy."
+
+        for r in results:
+            text += f"{r['title']}\n{r['body']}\n\n"
+
+        return text.strip()
+
+    except:
+        return "❌ Lỗi tìm kiếm."
