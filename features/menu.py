11aeb9b8dc0caf763930f748a6311dc7de24c7cf
Khanhkudo02 <113323275+Khanhkudo02@users.noreply.github.com>
2026-03-18 08:51:38 +0700

    Update menu.py

--- features/menu.py
+++ features/menu.py
@@ -1,7 +1,10 @@
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 def main_menu():
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    markup = ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        one_time_keyboard=False  # luôn hiển thị menu
+    )
 
     markup.row(
         KeyboardButton("🤖 Chat AI"),
